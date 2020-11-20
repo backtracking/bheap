3bc68867e298e77f9507eac37605980cd1383e6b
Baptiste Colin <baptiste.colin@outlook.fr>
2020-11-20 14:36:18 +0100

    Adds a module signature that can properly be reffered to by third party modules

--- binary_heap.ml
+++ binary_heap.ml
@@ -22,7 +22,53 @@ end
 
 exception Empty
 
-module Make(X : Ordered) = struct
+module type H = sig
+
+  (** Type of elements in the heap **)
+  type elt
+
+  (** Type of priority queues. *)
+  type t
+
+  (** [create ~dummy c] creates a new heap, with initial capacity of [c].
+      The value [dummy] is used to fill unused cells of the internal array.
+      Note: [dummy] can still be used as a regular value in the queue. *)
+  val create : dummy:elt -> int -> t
+
+  (** [length h] returns the number of elements of [h] *)
+  val length : t -> int
+
+  (** [is_empty h] checks the emptiness of [h] *)
+  val is_empty : t -> bool
+
+  (** [add x h] adds a new element [x] in heap [h]; size of [h] is doubled
+      when maximum capacity is reached; complexity $O(log(n))$ *)
+  val add : t -> elt -> unit
+
+  (** [minimum h] returns the minimum element of [h]; raises [Empty]
+      when [h] is empty; complexity $O(1)$ *)
+  val minimum : t -> elt
+
+  (** [remove h] removes the minimum element of [h]; raises [Empty]
+      when [h] is empty; complexity $O(log(n))$ *)
+  val remove : t -> unit
+
+  (** [pop_minimum h] removes the minimum element of [h] and returns it;
+      raises [Empty] when [h] is empty; complexity $O(log(n))$ *)
+  val pop_minimum : t -> elt
+
+  (** usual iterators and combinators; elements are presented in
+      arbitrary order *)
+  val iter : (elt -> unit) -> t -> unit
+
+  val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+
+end
+
+module Make(X : Ordered) : H with type elt = X.t = struct
+
+  (** Type of elements in the heap **)
+  type elt = X.t
 
   (* The heap is encoded in the array [data], where elements are stored
      from [0] to [size - 1]. From an element stored at [i], the left
@@ -30,8 +76,8 @@ module Make(X : Ordered) = struct
 
   type t = {
       mutable size : int;
-      mutable data : X.t array;
-             dummy : X.t;
+      mutable data : elt array;
+             dummy : elt;
            min_cap : int; (* minimal capacity, as given initially *)
     }
   (* invariant 0 <= size <= length data *)
